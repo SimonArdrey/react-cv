7eb29d15c1cb87132af42da483bb55388a46c46b
SimonArdrey <simonardrey@gmail.com>
2018-10-05 14:39:22 +0700

    Added headings for CV content section

---
+++ src/components/CVPage/components/General/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+import { HeadingText } from 'components/General';
+import LineHeading from 'components/LineHeading';
+
+const SectionLineHeading = styled(LineHeading).attrs({
+  lineColor: '#dedede',
+})`
+  color: #555;
+  font-weight: 700;
+  font-size: 1.4em;
+  margin: 1.5em 0 0.8em 0;
+`;
+
+export const SectionHeading = ({ children }) => (
+  <SectionLineHeading>
+    <HeadingText weight={700} color="#555">{children}</HeadingText>
+  </SectionLineHeading>
+);
+
+export const SummaryHeading = styled(LineHeading).attrs({
+  align: 'center',
+  lineColor: '#dedede',
+})`
+  font-size: 1.4em;
+  font-weight: 700;
+  color: #555;
+`;
--- src/components/CVPage/index.js
+++ src/components/CVPage/index.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import styled from 'styled-components';
-import LineHeading from 'components/LineHeading';
+import {
+  SummaryHeading,
+  SectionHeading,
+} from './components/General';
+import {
+  Text,
+  Center
+} from 'components/General';
 
 export default () => (
   <React.Fragment>
-    <SummaryHeading>Summary</SummaryHeading>
+    <section>
+      <SummaryHeading>Summary</SummaryHeading>
+      <Center>
+        <Text>
+          Ready to help your team; deliver an exceptional product.
+        </Text>
+        <Text style={{ textAlign: 'justify', width: '80%' }}>
+          Innovative and passionate full stack lead application developer. With broad experience working in a start-up with modern tech and tooling on large scale applications. Looking to sink my teeth into an exciting project where I can apply my knowledge and drive to make a real difference. Known as the detective who solves issues when others struggle. Credited with having good foresight and of being instrumental in driving advancement. Quick to master a deep fundamental understanding of unfamiliar tech and domains.
+        </Text>
+      </Center>
+    </section>
+    <SectionHeading>Professional Experience</SectionHeading>
   </React.Fragment>
 );
-
-const SummaryHeading = styled(LineHeading).attrs({
-  align: 'center',
-  lineColor: '#eee',
-})`
-  font-size: 1em;
-  font-weight: 400;
-  color: #444;
-`;
